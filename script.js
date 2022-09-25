19ed9fded02c46cca99e2f96b1d77a13db22a39a
khadar cadare <cadar@MacBook-Air.local>
2022-09-25 17:30:15 +0300

    removed popup

--- script.js
+++ script.js
@@ -6,22 +6,6 @@ hamburger.addEventListener('click', () => {
   navUl.classList.toggle('show');
 });
 
-// Popup on homepage after 2 seconds
-
-const popup = document.querySelector('.popup');
-const close = document.querySelector('.close');
-
-window.onload = () => {
-  setTimeout(() => {
-    popup.style.display = 'block';
-
-    // Add some time delay to show popup
-  }, 1500);
-};
-
-close.addEventListener('click', () => {
-  popup.style.display = 'none';
-});
 
 // Flipping words with color random
 var keywords = ['adventurous.', 'fantastic.', 'incredible.'];
