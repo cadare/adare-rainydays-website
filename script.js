e633e5f04a7f98206ed8bb54fb80316bdd0ebbba
khadar cadare <cadar@MacBook-Air.local>
2022-10-03 15:10:38 +0300

    cart created

--- script.js
+++ script.js
@@ -29,17 +29,46 @@ setInterval(function () {
   });
 }, 1500);
 
-var heart = document.getElementById('heart');
 
-heart.addEventListener('click', function () {
-  heart.style.color = '#66c144';
-});
+// Making a cart for the website Rainydays that is functional
 
-var addItemId = 0;
+// getting the target element
 
-function addToCart(item) {
-  addItemId += 1;
-  let selectedItem = document.createElement('div')
-  selectedItem.classList.add('cartImg')
 
+var myElement = document.querySelectorAll('.product__price');
+console.log(myElement)
+
+for (let i = 0; i < myElement.length; i++) {
+  var btnAddCart = document.createElement("button");
+  btnAddCart.className = 'addCart'
+  btnAddCart.innerHTML = "Add";
+
+  myElement[i].appendChild(btnAddCart).onclick = function () {
+    location.href = "/bag.html";
+  };
 }
+
+
+// var button = document.createElement("BUTTON")
+
+// button.innerText = "Button"
+
+// myElement.appendChild(button)
+
+
+// var heart = document.getElementById('heart');
+
+// heart.addEventListener('click', function () {
+//   heart.style.color = '#66c144';
+// });
+
+// var addItemId = 0;
+
+// function addToCart(item) {
+//   addItemId += 1;
+//   var selectedItem = document.createElement('div')
+//   selectedItem.classList.add('cartImg')
+//   selectedItem.setAttribute('id', addItemId);
+//   var cartItems = document.getElementById('')
+
+// }
