46f01c9d53aae8141e8490e64ca7e58961fddd35
khadar cadare <cadar@MacBook-Air.local>
2022-12-20 16:10:32 +0300

    ttester

--- js/products/fetchProducts.js
+++ js/products/fetchProducts.js
@@ -1,4 +1,5 @@
-const url = 'https://brbforum.com/wp-json/wc/store/products?consumer_key=ck_f594e7cc7bc4a0174b4687b924a966264f7bf578&consumer_secret=cs_b13b4fd6e8bc3e176bb2e74a3c4ded2f8e3c4fea'
+const url = "https://brbforum.com/wp-json/wp/v2/posts?consumer_key=ck_f594e7cc7bc4a0174b4687b924a966264f7bf578&consumer_secret=cs_b13b4fd6e8bc3e176bb2e74a3c4ded2f8e3c4fea"
+
 
 
 const productContainer = document.querySelector('.productPage');
@@ -20,24 +21,35 @@ function displayProducts(products) {
 
 
     products.forEach(function (product) {
-        productContainer.innerHTML += ` 
-    <div class='product-fetch'>
-    
-    <div class="products"><h2>${product.name}</h2>
-    <div class="product-image_fetch" style="background-image:url('${product.images[0].src }')"></div>
+        productContainer.innerHTML +=
+            //     ` 
+            // <div class='product-fetch'>
 
-    
-    </div>
-    </div>`
+            // <div class="products"><h2>${product.name}</h2>
+            // <div class="product-image_fetch" style="background-image:url('${product.images[0].src }')"></div>
+
+
+            // </div>
+            // </div>`
+
+            `<a href="product-item.html?id=${product.id}" class="card">
+         
+           
+            <h1 class="title">${product.title.rendered}<h1/>
+            
+            
+            <p class="paragrpy">${product.excerpt.rendered}<p/>
+          
+                                        
+                                                                                                                                                                                                       
+                                          
+                                            </a>`;
+
+
+
+
+
 
 
     })
 }
-// var json = JSON.stringify(product)
-// productShow.innerHTML = `  <div class="products"><h2>${json}</h2>`
-
-// const productSingleItem = document.querySelector('.product-item-on-itemPage');
-
-// console.log('hello2')
-
-// productSingleItem.style.backgroundColor = 'red';
